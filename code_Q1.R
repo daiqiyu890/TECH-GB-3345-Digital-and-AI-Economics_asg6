343492134043f7a9b8fb12325ec62204d4e1235b
Dai Qiyu <83342930+daiqiyu890@users.noreply.github.com>
2026-03-30 11:29:19 -0400

    upload code

---
+++ code_Q1.R
@@ -0,0 +1,9 @@
+# TECH-GB 3345 (Digital and AI Economics)
+# Submitted to: Prof. Arun Sundararajan
+# Submitted by: Qiyu, Ilias
+
+# Some preliminary commands
+rm(list = ls()) # clearing the workspace
+library(data.table) # loading some needed packages
+library(tidyverse)
+library(fixest)
